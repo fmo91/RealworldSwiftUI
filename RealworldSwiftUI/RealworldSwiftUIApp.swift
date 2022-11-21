c2981f28c98fc2c67621d7ac3571a9d7a4703d10
Fernando Ortiz <ortizfernandomartin@gmail.com>
2022-11-21 12:48:46 -0300

    First commit

---
+++ RealworldSwiftUI/RealworldSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealworldSwiftUIApp.swift
+//  RealworldSwiftUI
+//
+//  Created by Fernando Ortiz on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct RealworldSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
